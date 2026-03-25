011710a5e2b55d94d35a0888020b924f8aa23872
Chiranjeevi R <chiranjeevir.mca25@rvce.edu.in>
2026-03-25 22:40:57 +0530

    6th

---
+++ Functions.py
@@ -0,0 +1,8 @@
+line = "chiru and shri LAKSHMI"
+print(len(line))    #Lenght of the Stings
+print(line.find("shri"))    #Finds the starting index of the Strings
+print(line.upper())     #Convert Uppercase
+print(line.lower())     #Convert Lowercase
+print(line.replace("and","hate"))   #Repalce the string
+print("chiru" in line)      #Boolean values True/False
+print("LAKSHMI".title())    #Convert the First letter capital other Small
