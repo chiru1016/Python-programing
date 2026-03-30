3797528a49c6cbac1180186a24acd80e832b149c
Chiranjeevi R <chiranjeevir.mca25@rvce.edu.in>
2026-03-30 18:22:13 +0530

    if

---
+++ if condition.py
@@ -0,0 +1,20 @@
+#boolean
+#p1
+hot = False
+cold = True
+
+if hot:
+    print("drink water")
+elif cold:
+    print("wear sweater")
+else:
+    print("lovely day")
+
+
+#p2
+house_price = 100000
+good_credit = False
+if good_credit:
+    print(f"you need to put {house_price * .10} dollars ")
+else:
+    print(f"you need to put {house_price * .20} dollars ")
