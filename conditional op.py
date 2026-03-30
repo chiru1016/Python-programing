8ee8aca789c1dbd00645f2d962df53155f2a520e
Chiranjeevi R <chiranjeevir.mca25@rvce.edu.in>
2026-03-30 18:45:56 +0530

    con

---
+++ conditional op.py
@@ -0,0 +1,8 @@
+
+temperature = input("Please enter your temperature in celsius:  ")
+if temperature > "30":
+    print(f"its a hot day bcz your temperature is {temperature} celsius.")
+elif temperature < "30":
+    print(f"its a cold day bcz your temperature is {temperature} celsius.")
+else:
+    print(f"its a fine day bcz your temperature is {temperature} celsius.")
