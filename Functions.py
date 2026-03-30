ee504b1e6749033c9946d9b30de7da460df4d371
Chiranjeevi R <chiranjeevir.mca25@rvce.edu.in>
2026-03-30 12:13:35 +0530

    add

--- Functions.py
+++ Functions.py
@@ -1,4 +1,5 @@
 line = "chiru and shri LAKSHMI"
+x=2.9
 print(len(line))    #Lenght of the Stings
 print(line.find("shri"))    #Finds the starting index of the Strings
 print(line.upper())     #Convert Uppercase
@@ -6,3 +7,5 @@ print(line.lower())     #Convert Lowercase
 print(line.replace("and","hate"))   #Repalce the string
 print("chiru" in line)      #Boolean values True/False
 print("LAKSHMI".title())    #Convert the First letter capital other Small
+print(round(x))     #round up
+print(abs(-2.9))  #makes positive
