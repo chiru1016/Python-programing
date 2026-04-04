c0d05e94baef8db381f12b9de1c80c95c95a9fea
Chiranjeevi R <chiranjeevir.mca25@rvce.edu.in>
2026-04-04 08:37:04 +0530

    new

--- pound to kilogram.py
+++ pound to kilogram.py
@@ -1,3 +1,10 @@
-weight = input("wt is your weight? (in pound) ")
-kilogram = float(weight) /2
-print(kilogram)
+w = input("wt is your weight? ")
+parameter = input("(L)bs or (K)g?? ")
+if parameter.upper()=="K":
+    weight = float(w) *2
+    print(f"weight is {weight}pounds ")
+elif parameter.upper()=="L":
+    weight = float(w) /2
+    print(f"weight is {weight}KG")
+else:
+    print("invallid")
