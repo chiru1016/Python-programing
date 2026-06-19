e8e07c7ed250565deff91dd4756689f8be1f47e7
Chiranjeevi R <246724548+chiru1016@users.noreply.github.com>
2026-06-19 21:27:15 +0530

    shri

--- Altitude.py
+++ Altitude.py
@@ -1,7 +1,7 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        high = 0
-        low = 0
+        high = 0 # highest point
+        low = 0 # lowest point
         for i in gain:
             low += i
             high = max(high,low)
